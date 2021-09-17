1257e4692b7bc4f5aa2c9ae83b4324cdc445a574
unknown <fajarmh2@gmail.com>
2021-09-17 21:59:50 +0700

    Already Install Material UI React Version 4

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
         <div className = "header">
-            <h2>I am a Header</h2>
+            <h2>I am a Header Vroh</h2>
+            <PersonIcon />
         </div>
     )
 }
