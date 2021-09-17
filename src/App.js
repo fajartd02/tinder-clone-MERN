303ce37849fcd7f8bb237169a95ec51bfa38db28
unknown <fajarmh2@gmail.com>
2021-09-17 18:13:10 +0700

    New Start Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+    {/* Header */}
+    <Header/>
+
+    {/* TinderCards */}
+    {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
