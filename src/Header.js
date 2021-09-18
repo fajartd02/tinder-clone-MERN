ac3ce222fc6ab4884cd03ccc6b02b224716a95f0
unknown <fajarmh2@gmail.com>
2021-09-19 00:42:41 +0700

    complete Navbar UI

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
         <div className = "header">
-            <h2>I am a Header Vroh</h2>
-            <PersonIcon />
+            <IconButton>
+                <PersonIcon fontSize ="large" className = "header__icon" />
+            </IconButton>
+
+            <img 
+            className = "header__logo"
+            src = "/images/tinder.png"
+            alt = "Tinder Logo"
+            />
+
+            <IconButton>
+                <ForumIcon fontSize ="large" className = "header__icon"/>
+            </IconButton>    
         </div>
     )
 }
